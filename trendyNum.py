a06183632e0994cd916f71b3747a854c38e75464
Swetha P V <swethapv03@gmail.com>
2024-10-03 13:54:23 +0530

    Create trendyNum.py

---
+++ trendyNum.py
@@ -0,0 +1,23 @@
+'''
+Write a program to check whether the given number is a trendy number or not. A number is said to be a trendy number if and only if it has 3 digits and the middle digit is divisible by 3.
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+If the given number is a trendy number, then print "Trendy Number". Otherwise, print "Not a Trendy Number".
+Refer the sample output for formatting.
+Sample Input:
+791
+Sample Output:
+Trendy Number
+'''
+
+n=int(input())
+if(n>=100 and n<=999):
+    a=n//10
+    b=a%10
+    if(b%3==0):
+        print("Trendy Number")
+    else:
+        print("Not a Trendy number")
+else:
+    print("Not a Trendy Number")
